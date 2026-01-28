2a0d867c4036ae91c6e22257245d69825d2104ed
GermanoMan0el <germanomanoel007@gmail.com>
2026-01-28 18:48:20 -0300

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabneews
+
 Projeto para estudos do curso "curso.dev" onde vamos implementar um clone do site https://www.tabnews.com.br/ junto com instrutor @filipedeschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return (
-        <h1>Lewis Hamilton é melhor piloto de formula 1 do mundo. 👑</h1>
-    )
+  return <h1>Hello World!!!</h1>;
 }
 
 export default Home;
